c2383fa5b9db619adc5770e7ec2d80dd3ceaa16d
Zack Macharia <blaqhrse@gmail.com>
2019-05-17 02:01:00 -0500

    removed hardcoded api key

--- add_ip_address/add_ip_address.py
+++ add_ip_address/add_ip_address.py
@@ -1,5 +1,6 @@
 from urllib import request
 import ssl
+from api_key import pa220key
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -14,11 +15,10 @@ ctx.verify_mode = ssl.CERT_NONE
 def add_ip_address():
     xpath = "&type=config&action=set&xpath=/config/devices/entry"\
         "[@name='localhost.localdomain']/vsys/entry[@name='vsys1']"\
-        "/address/entry[@name='test3']&"\
-        "element=<ip-netmask>1.1.1.3/32</ip-netmask>"
+        "/address/entry[@name='test4']&"\
+        "element=<ip-netmask>1.1.1.4/32</ip-netmask>"
     host = '192.168.1.128'
-    api_key = 'LUFRPT1UQVZsd29wMTRTVlZGcmZJWkFFUFpETkV0c1U9bmNoV2Z6al'\
-        'FRTFV2SkY2UDFkRFRnZUtRV3hUQjZrR0orUklLU1lQMVFVOD0='
+    api_key = str(pa220key)
     key = '?key=' + api_key
     add_ip_address = 'https://' + host + '/api/' + key + xpath
     fh = request.urlopen(add_ip_address, context=ctx)
