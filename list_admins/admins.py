b0afef6c42ae997287024973c92ac5d80d0f8f38
Zack Macharia <blaqhrse@gmail.com>
2019-05-19 01:53:42 -0500

    reformated the output for admin and their respective permissions in a colon separated format

--- list_admins/admins.py
+++ list_admins/admins.py
@@ -13,17 +13,19 @@ def get_admins():
     for user in users.iter('entry'):  # iterating through users children
         # print(user.tag, user.attrib)  # output validation
         admin_name = user.get('name')  # using get due to assignment = value
-        print(admin_name)  # output validation
+        # print(admin_name)  # output validation
         for permission in user.iter('permissions'):
             for role_base in permission.iter('role-based'):
                 for role in role_base:
                     if role.tag == 'superuser':
-                        print('superuser')
+                        admin_role = 'superuser'
+                        # print(admin_role)
                     else:
                         for custom_role in role.iter('custom'):
                             for profile in custom_role.iter('profile'):
                                 admin_role = profile.text
-                                print(admin_role)
+                                # print(admin_role)
+        print(admin_name, ':', admin_role)
 
 
 get_admins()
