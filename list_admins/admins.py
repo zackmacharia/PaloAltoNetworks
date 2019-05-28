276099cb047a26f4b63002ccf191ae9985b9e66c
Zack Macharia <blaqhrse@gmail.com>
2019-05-27 19:40:52 -0500

    refactored code into a function

--- list_admins/admins.py
+++ list_admins/admins.py
@@ -4,17 +4,21 @@ filename = input('Enter a configuration file name: ')
 tree = ET.parse(filename)
 
 
-for elem in tree.iter():
-    if 'users' in elem.tag:
-        users_node = elem
-        for user in users_node.iter('entry'):
-            admin_name = user.get('name')
-            for options in user.iter('role-based'):
-                role_base_node = options
-                for role in role_base_node.iter():
-                    if 'superuser' in role.tag:
-                        if role.text == 'yes':
-                            role = 'Super User'
-                    elif 'profile' in role.tag:
-                        role = role.text
-            print('Administrator: ', admin_name, '-', 'Role: ', role)
+def get_admin_info():
+    for elem in tree.iter():
+        if 'users' in elem.tag:
+            users_node = elem
+            for user in users_node.iter('entry'):
+                admin_name = user.get('name')
+                for options in user.iter('role-based'):
+                    role_base_node = options
+                    for role in role_base_node.iter():
+                        if 'superuser' in role.tag:
+                            if role.text == 'yes':
+                                role = 'Super User'
+                        elif 'profile' in role.tag:
+                            role = role.text
+                print('Administrator: ', admin_name, '-', 'Role: ', role)
+
+
+get_admin_info()
