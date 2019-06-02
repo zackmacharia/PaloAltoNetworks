1c5c73fac6179fd620904e9da110e8b00f423735
Zack Macharia <blaqhrse@gmail.com>
2019-06-02 16:29:41 -0500

    new script to enumerate security and nat policies on a PaloAlto Networks firewall

---
+++ policies/enum_policies.py
@@ -0,0 +1,34 @@
+import xml.etree.cElementTree as ET
+import logging
+
+filename = input('Enter a configuration file name: ')
+tree = ET.parse(filename)
+
+
+def enumerate_sec_rules():
+    "Function enumerates the security policies configured\
+    on a Palo Alto Networks Firewall"
+    logging.info('Enumerating security policies -- started')
+    for security in tree.iter('security'):  # starts loop at the security tag
+        for rules in security.iter('rules'):
+            for entry in rules.iter('entry'):
+                print(entry.tag, entry.attrib)
+    logging.info('Enumerating security policies -- finished')
+
+
+def enumerate_nat_rules():
+    "Function enumerates the nat policies configured\
+    on a Palo Alto Networks Firewall"
+    logging.info('Enumerating nat policies -- started')
+    for nat in tree.iter('nat'):  # starts loop at the nat tag
+        for rules in nat.iter('rules'):
+            for entry in rules.iter('entry'):
+                print(entry.tag, entry.attrib)
+    logging.info('Enumerating nat policies -- ended')
+
+
+if __name__ == '__main__':
+    # print('Security Policies')
+    enumerate_sec_rules()
+    # print('\n', 'Nat Policies')
+    enumerate_nat_rules()
