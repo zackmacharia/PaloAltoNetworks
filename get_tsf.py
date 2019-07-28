e4e11fb06e99d0ea47ea313f6662893711ad01f8
Zack Macharia <blaqhrse@gmail.com>
2019-07-28 14:46:52 -0500

    removed hardcoded API key and used key import instead

--- get_tsf.py
+++ get_tsf.py
@@ -1,10 +1,10 @@
 import requests
+import key
 
 url = '10.200.200.1'
-api_path = '/api/?type=op&cmd=<request><tech-support><dump></dump></tech-support></request>'
-key = 'LUFRPT10VGJKTEV6a0R4L1JXd0ZmbmNvdUEwa25wMlU9d0N'\
-    '5d292d2FXNXBBeEFBUW5pV2xoZz09'
-full_url = 'https://' + url + api_path + '&key=' + key
+api_path = '/api/?type=op&cmd=<request><tech-support><dump></dump>'\
+    '</tech-support></request>'
+full_url = 'https://' + url + api_path + '&key=' + key.key
 response = (requests.get(full_url, verify=False))
 print(response.status_code)
 # print(full_url)
