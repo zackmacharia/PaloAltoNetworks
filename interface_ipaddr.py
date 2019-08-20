272029a110473b356f02af3599c532b2ffd6ad85
Zack Macharia <zmacharia@dfwmac191jhd4.paloaltonetworks.local>
2019-08-19 20:49:50 -0500

    fixed apikey variable to reference a function(keys.pa_vm_a() instead of keys.pa_vm_a

--- interface_ipaddr.py
+++ interface_ipaddr.py
@@ -3,14 +3,25 @@ import ipaddress
 import requests
 import keys
 
-print('testing git')
+
+while True:
+    try:
+        host = input("Enter Firewall's IP Address: ")
+        ipaddress.ip_address(host) # validate user input is a valid IP Address
+        break
+    except:
+        print('Error! Please enter a valid IP Address.')
+
+int_no = input("Enter Firewall's Ethernet Port Number: ")
+
 
 def sub_int_ipaddr():
     """Get Subinterfac IP Address from Palo Alto Firewall"""
     interface = "\'" +  'ethernet1/' + int_no + "\'" # sub-interface with single quotes per API
     split_int = interface.split('.')
     main_int = split_int[0] + "\'"  # physical interface with single quote per API
-    apikey = keys.homefw2_key
+    # apikey = keys.homefw2_key
+    apikey = keys.pa_vm_a()
     xpath = "/api/?type=config&action=get&xpath=/config/devices/entry[@name="\
                  "'localhost.localdomain']/network/interface/ethernet/entry[@name=" + main_int + \
                  "]/layer3/units/entry[@name=" + interface + "]/ip&key=" + apikey
@@ -32,7 +43,8 @@ def sub_int_ipaddr():
 def int_ipaddr():
     """Get Physical Interfac IP Address from Palo Alto Firewall"""
     interface = "\'" +  'ethernet1/' + int_no + "\'"
-    apikey = keys.homefw2_key
+    # apikey = keys.homefw2_key
+    apikey = keys.pa_vm_a()
     xpath = "/api/?type=config&action=get&xpath=/config/devices/entry[@name="\
                  "'localhost.localdomain']/network/interface/ethernet/entry[@name=" + interface + "]/layer3/ip" \
                 "&key="+ apikey
