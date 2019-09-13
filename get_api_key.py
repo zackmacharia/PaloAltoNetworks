04259db443663333c6b45557e5ee992dfb9544f5
Zack Macharia <zmacharia@dfwmac191jhd4.paloaltonetworks.local>
2019-09-12 19:14:32 -0500

    introduced use of getpass to mask password entry

--- get_api_key.py
+++ get_api_key.py
@@ -1,4 +1,5 @@
 import requests
+import getpass
 import re
 
 
@@ -7,12 +8,12 @@ def get_api_key():
     Important: change the user and password values in the lines below"""
     host = input('Enter hostname or IP: ')
     username = input('Enter username: ')
-    password = input('Enter password: ')
+    password = getpass.getpass(prompt='Enter password: ')
     req_key = 'https://' + host + \
         '/api/?type=keygen&user=' + username + '&password=' + password  # creds
     data = requests.get(req_key, verify=False)
     data_string = data.text
-    pattern = re.compile(r"<key>(.*?)</key>") 
+    pattern = re.compile(r"<key>(.*?)</key>")
     search = re.search(pattern, data_string)
     key = search.group(1) # targeting group with API key information
     print(key)
